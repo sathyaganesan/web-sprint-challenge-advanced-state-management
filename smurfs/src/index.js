78e7ede6894c5a49e78b79a0d0f6cfbcc624e2da
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-10-24 12:20:15 -0400

    Done with Project setup and Index file

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'react-thunk';
+import {Provider} from 'react-redux'
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { AppReducer } from './Reducers/AppReducer';
+
+const logger = ({ getState }) => (next) => (action) => {
+    console.log("Dispatching action: ", action);
+    next(action);
+};
+  
+const store = createStore(AppReducer, applyMiddleware(logger, thunk))
+
+ReactDOM.render(
+    <Provider store = {store}>
+         <App />
+    </Provider>,
+    document.getElementById("root")
+);
